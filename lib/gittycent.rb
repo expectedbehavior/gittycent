32b4d1fd9b6a45f9858056ae44f79faecee59c47
Elijah Miller <elijah.miller@gmail.com>
2010-04-18 14:47:50 -0400

    Documentation of most methods.

--- lib/gittycent.rb
+++ lib/gittycent.rb
@@ -10,6 +10,10 @@ class GitHub
   
   attr_accessor :options
   
+  # Connects to GitHub using login/token. Yields or returns the new 
+  # connection.
+  #
+  #  GitHub.connect(:login => 'jqr', :token => 'somesecretstuff123')
   def self.connect(options)
     connection = new(options)
     if block_given?
@@ -19,20 +23,33 @@ class GitHub
     end
   end
   
+  # Simplified method for connecting with git config specified credentials.
+  # Also allows for using an alternate section for easy handling of multiple
+  # credentials. Just like connect this yields or returns the new connection.
+  #
+  #  github = GitHub.connect_with_git_config
   def self.connect_with_git_config(section = :github, options = {}, &block)
     config = Hash[*`git config -l`.scan(/^(.*?)=(.*)$/).flatten]
     connect(options.merge(:login => config["#{section}.user"], :token => config["#{section}.token"]), &block)
   end
   
+  # Returns a new GitHub connection object, requires :login and :token 
+  # options.
+  #
+  #  github = GitHub.new(:login => 'jqr', :token => 'somesecretstuff123')
   def initialize(options)
     self.options = options
     debug "Login using login #{options[:login]} with token #{options[:token]}"
   end
   
+  # Returns the verbosity level.
   def verbosity
     options[:verbosity] || NONE
   end
   
+  # Finds a GitHub user by login.
+  #
+  #  user = github.user('jqr')
   def user(login)
     if login == options[:login]
       AuthenticatedUser.new(self, :login => options[:login])
@@ -41,15 +58,17 @@ class GitHub
     end
   end
   
+  # Finds GitHub users by a query string.
   def user_search(query)
     get("/user/search/#{query}")['users'].map { |u| User.new(self, u) }
   end
 
+  # Returns the currently authenticated user.
   def authenticated_user
     @authenticated_user ||= user(options[:login])
   end
   
-  def inspect
+  def inspect # :nodoc:
     "#<#{self.class}#{' ' + options[:login] if options[:login].present?}>"
   end
   
@@ -62,6 +81,9 @@ class GitHub
     }
   end
   
+  # Performs a GET request on path, automatically prepending api version and 
+  # format paramters. Will automatically retry if the request rate limiting is 
+  # hit.
   def get(path)
     with_retry do
       url = "http://github.com/api/#{API_VERSION}/#{FORMAT}#{path}"
@@ -74,6 +96,9 @@ class GitHub
     end
   end
   
+  # Performs a POST request on path, automatically prepending api version and 
+  # format paramters. Will automatically retry if the request rate limiting is 
+  # hit.
   def post(path, params = {})
     with_retry do
       url = "http://github.com/api/#{API_VERSION}/#{FORMAT}#{path}"
@@ -99,6 +124,8 @@ class GitHub
     debug object.pretty_inspect
   end
   
+  # Automatically retries a block of code if the returned value is rate 
+  # limited (HTTP Code 403) by GitHub.
   def with_retry
     count = 0
     loop do
@@ -168,28 +195,36 @@ class GitHub
       login.to_s
     end
     
+    # Returns this user's repositories.
     def repos
       @repos ||= get("/repos/show/#{login}")['repositories'].map { |r| Repo.new(connection, r) }
     end
     
+    # Returns a list of all public repos thi user is watching.
     def watched_repos
       @repos ||= get("/repos/watched/#{login}")['repositories'].map { |r| Repo.new(connection, r) }
     end
     
-    # supported options include: :name, :description, :homepage, :public
+    # Adds a new repository for this user. Supported options include: :name, 
+    # :description, :homepage, :public.
+    #
+    #  repo = user.create_repo(:name => 'hello', :public => false)
     def create_repo(options)
       options = options.dup
       Repo.new(connection, post("/repos/create", options)['repository'])
     end
     
+    # Loads lazily-fetched attributes.
     def load
       @attributes = get("/user/show/#{login}")['user'].symbolize_keys
     end
     
+    # Returns all the users that follow this user.
     def followers
       @followers ||= get("/user/show/#{login}/followers")['users'].map { |u| User.new(connection, :login => u) }
     end
 
+    # Returns all the users that this user is following.
     def following
       @following||= get("/user/show/#{login}/following")['users'].map { |u| User.new(connection, :login => u) }
     end
@@ -201,6 +236,10 @@ class GitHub
       :total_private_repo_count, :public_repo_count, :location, :email, 
       :collaborators, :public_gist_count, :blog, :name, :following_count, 
       :disk_usage
+      
+    # Returns a list of all repos this user is watching.
+    def watched_repos
+    end
   end
   
   class Repo < Connectable
@@ -214,14 +253,17 @@ class GitHub
       name.to_s
     end
     
+    # Returns the owner of this repository.
     def owner
       @owner ||= User.new(connection, :login => @attributes[:owner])
     end
     
+    # Returns a list of all collaborators.
     def collaborators
       @collaborators ||= get("/repos/show/#{owner.login}/#{name}/collaborators")['collaborators'] || []
     end
     
+    # Sets the list of collaborators.
     def collaborators=(value)
       value = value.dup.uniq
       removals = collaborators - value
@@ -237,6 +279,8 @@ class GitHub
       @collaborators = nil
     end
     
+    # Returns the shell commands used to do an initial push of this project to
+    # GitHub.
     def initial_push_command
       "git remote add origin git@github.com:#{owner.login}/#{name}.git &&\n" +
       "git push origin master &&\n" +
@@ -244,22 +288,27 @@ class GitHub
       "git config --add branch.master.merge refs/heads/master" 
     end
     
+    # Returns repositories in this repository's network.
     def network
       @network ||= get("/repos/show/#{owner.login}/#{name}/network")['network'].map { |r| Repo.new(connection, r) }
     end
     
+    # Returns the languages detected.
     def languages
       get("/repos/show/#{owner.login}/#{name}/languages")['languages']
     end
     
+    # Loads lazily-fetched attributes.
     def load
       @attributes = get("/repos/show/#{owner.login}/#{name}")['repository'].symbolize_keys
     end
     
+    # Returns all tags of this repository.
     def tags
       get("/repos/show/#{owner.login}/#{name}/tags")['tags']
     end
     
+    # Returns all branches of this repository.
     def branches
       get("/repos/show/#{owner.login}/#{name}/branches")['branches']
     end
